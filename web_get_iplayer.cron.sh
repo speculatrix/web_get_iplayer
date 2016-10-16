2c1a3eb4fe5e44736d3b628a4825ab0be82be313
Paul Mansfield <paul-github@mansfield.co.uk>
2016-10-16 16:32:20 +0000

    updating comments to be more helpful

--- web_get_iplayer.cron.sh
+++ web_get_iplayer.cron.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# the script should be called by cron or other schedular, and it drives
+# the web_get_iplayer.py queue running process
+# you need to customise it with the location of the web_get_iplayer.py
+# script
 
 OUTFILE=/var/lib/web_get_iplayer/web_get_iplayer.out
 
