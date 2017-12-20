3dc3c4b8d10ec61e4ac2af2ad540b728eed69cbc
speculatrix <paul-github@mansfield.co.uk>
2017-12-20 16:20:21 +0000

    bbc iplayer radio format is m4a but want mp3

---
+++ m4a-to-mp3.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+INFILE="$1"
+
+if [ "$INFILE" == "" ] ; then
+	echo "no input file specified"
+	exit
+fi
+
+OUTFILE=`echo $INFILE | sed 's/m4a/mp3/g'`
+
+if [ "$INFILE" == "$OUTFILE" ] ; then
+	echo "name transformation failed, $INFILE to $OUTFILE"
+fi
+
+if [ "$OUTFILE" == "" ] ; then
+	echo "name transformation failed, OUTFILE blank"
+fi
+
+if [ ! -f "$INFILE" ] ; then
+	echo "input file doesn't exist"
+fi
+
+if [ -f "$OUTFILE" ] ; then
+	echo "output file already exists"
+fi
+
+
+#nice ffmpeg --enable-libfdk-aac --enable-nonfree -i "$INFILE" -ab 320k -map_metadata 0 "$OUTFILE"
+#nice ffmpeg --enable-nonfree -i "$INFILE" -ab 320k -map_metadata 0 "$OUTFILE"
+nice ffmpeg -i "$INFILE" -ab 320k -map_metadata 0 "$OUTFILE"
+
+
