d4b96b7ed1ec9c1ef58f2ea12c49e3c3c2a2378e
speculatrix <paul-github@mansfield.co.uk>
2017-12-20 16:18:12 +0000

    fixed scaling option

--- ts-to-mp4.sh
+++ ts-to-mp4.sh
@@ -1,3 +1,36 @@
-#/bin/bash
-ffmpeg -i $1 -acodec copy -vcodec copy $2
+#!/bin/bash
+
+scale=""
+if [ "$1" == "-s" ] ; then
+	echo "scaling to $2"
+	scale="-vf scale=$2"
+	shift
+	shift
+fi
+
+if [ "$1" == "" ] ; then
+	echo "no input file given"
+	exit 1
+fi
+
+if [ "$2" == "" ] ; then
+	echo "no output file given"
+	exit 1
+fi
+
+if [ ! -f "$1" ] ; then
+	echo "input file doesn't exist"
+	exit 1
+fi
+
+if [ -f "$2" ] ; then
+	echo "output file already exists"
+	exit 1
+fi
+
+if [ "$scale" == "" ] ; then
+	time nice ffmpeg -i $1		-c:a copy -c:v copy		$2
+else
+	time nice ffmpeg -i $1 $scale	-c:v libx264 -profile:v baseline $2
+fi
 
