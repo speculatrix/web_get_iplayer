9389c88024423b4344835b51c47d4497541684f9
speculatrix <paul-github@mansfield.co.uk>
2017-12-22 14:24:55 +0000

    added some error checking

--- m4a-to-mp3.sh
+++ m4a-to-mp3.sh
@@ -4,30 +4,37 @@ INFILE="$1"
 
 if [ "$INFILE" == "" ] ; then
 	echo "no input file specified"
-	exit
+	exit 1
 fi
 
-OUTFILE=`echo $INFILE | sed 's/m4a/mp3/g'`
+OUTFILE=`echo $INFILE | sed 's/m4a$/mp3/g'`
 
 if [ "$INFILE" == "$OUTFILE" ] ; then
 	echo "name transformation failed, $INFILE to $OUTFILE"
+	exit 1
 fi
 
 if [ "$OUTFILE" == "" ] ; then
 	echo "name transformation failed, OUTFILE blank"
+	exit 1
 fi
 
 if [ ! -f "$INFILE" ] ; then
 	echo "input file doesn't exist"
+	exit 1
 fi
 
 if [ -f "$OUTFILE" ] ; then
 	echo "output file already exists"
+	exit 1
 fi
 
 
 #nice ffmpeg --enable-libfdk-aac --enable-nonfree -i "$INFILE" -ab 320k -map_metadata 0 "$OUTFILE"
 #nice ffmpeg --enable-nonfree -i "$INFILE" -ab 320k -map_metadata 0 "$OUTFILE"
-time nice ffmpeg  -loglevel warning -i "$INFILE" -ab 320k -map_metadata 0 "$OUTFILE"
+errcode time nice ffmpeg  -loglevel warning -i "$INFILE" -ab 320k -map_metadata 0 "$OUTFILE"
 
+if [ $errcode -ne 0 ] ; then
+	echo "ffmpeg returned error code $errcode"
+fi
 
